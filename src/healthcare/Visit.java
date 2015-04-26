c0fe77b303d513b8ab40ebc25696734c4799730a
YeWang555 <ywang555@asu.edu>
2015-04-26 12:55:21 -0700

    Update Visit.java

--- src/healthcare/Visit.java
+++ src/healthcare/Visit.java
@@ -7,11 +7,56 @@ package healthcare;
 
 /**
  *
- * @author Jing Liang
+ * @author Ye
  */
 public class Visit {
-    public Visit()
+    
+    private String doc1;
+    private String date2,pat,hospital,treatment;
+    
+    
+   public Visit(String newDoc, String newPat)
+   {
+     doc1=newDoc;
+     pat=newPat;
+   }
+   
+    public void setPatient(String pat1)
     {
-        
+    pat=pat1;
     }
+   public void setDoctor(String do1)
+   {
+       doc1=do1;
+   }
+   public String getDoctor()
+    {
+         return doc1;     
+     }
+     public String getDate()
+{
+         return date2;
 }
+     public void setDate(String newDat)
+{
+          date2=newDat;
+}
+     public void setTreatment(String t)
+     {
+                   treatment=t; 
+     }
+     public String getTreatment()
+     {
+         return treatment;
+     }
+     public void setHospital(String hp)
+     {
+                  hospital=hp; 
+     }
+     public String getHospital()
+     {
+         return hospital;
+     }
+}
+
+
